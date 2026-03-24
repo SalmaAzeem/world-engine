09eccf3991a5d6553323c23dec8902065e5966c1
SalmaAzeem <s-salma.azeem@zewailcity.edu.eg>
2026-03-24 23:42:44 +0200

    refactor: change lighting and hvac mode

--- wokwi/main.py
+++ wokwi/main.py
@@ -11,6 +11,9 @@ EPOCH_OFFSET = 946684800
 dht_sensor = dht.DHT22(Pin(18))
 pir = Pin(14, Pin.IN)
 
+current_hvac_mode = "ECO"
+current_lighting_dimmer = 0
+
 def wifi_connection():
     net = network.WLAN(network.STA_IF)
     net.active(True)
@@ -33,33 +36,38 @@ room = 502
 sensor_id = f"b{building}-f{floor}-r{room}"
 
 def handle_command(topic, msg):
+    global current_hvac_mode, current_lighting_dimmer
+    
     try:
         payload = ujson.loads(msg)
         target_device = payload.get("target_device")
         action = payload.get("action")
         value = payload.get("value")
-        print("subscribed topic: ", topic)
-        print(sensor_id)
+        
+        print(f"subscribed topic: {topic}")
+        
         if target_device != sensor_id:
-
             print("invalid device")
             return
+            
         if action == "set_hvac":
-            if value not in ["ON", "OFF", "ECO"]:
-                return
-            print("action is: ", action, "and value is: ", value)
-            # do something
+            if value in ["ON", "OFF", "ECO"]:
+                current_hvac_mode = value
+            else:
+                print("invalid HVAC value")
+                
         elif action == "set_lighting":
             try:
                 value_light = int(value)
                 if 0 <= value_light <= 100:
-                    print("light dimmed")
+                    current_lighting_dimmer = value_light
                 else:
                     print("value outside range")
             except ValueError:
-                print("rejected")
+                print("rejected: lighting value must be an integer")
         else: 
             print(f"rejected action: {action}")
+            
     except ValueError:
         print("rejected json payload")
 
@@ -67,11 +75,12 @@ def main():
     wifi_connection()
 
     client = MQTTClient("phase1", "broker.hivemq.com")
-
     client.set_callback(handle_command)
     client.connect()
-    client.subscribe(f"campus/bldg_{building}/floor_{floor:02d}/room_{room}/command")
-    print("mqtt connected")
+    
+    command_topic = f"campus/bldg_{building}/floor_{floor:02d}/room_{room}/command"
+    client.subscribe(command_topic)
+    print("mqtt connected and subscribed to:", command_topic)
 
     last_published = time.ticks_ms() - 5000
     interval = 5000
@@ -79,6 +88,7 @@ def main():
     while True:
         try:
             client.check_msg()
+            
             if time.ticks_diff(time.ticks_ms(), last_published) >= interval:
                 dht_sensor.measure()
                 temp = dht_sensor.temperature()
@@ -87,6 +97,7 @@ def main():
 
                 validated = validations(temp, humidity)
                 light_level = 800 if motion else 50
+                
                 if validated:
                     payload = {
                         "metadata": {
@@ -103,19 +114,17 @@ def main():
                             "light_level": light_level
                         },
                         "actuators": {
-                            "hvac_mode": "ECO",
-                            "lighting_dimmer": 100 if motion else 0
+                            "hvac_mode": current_hvac_mode,
+                            "lighting_dimmer": current_lighting_dimmer
                         }
                     }
                     json_payload = ujson.dumps(payload)
                     client.publish(f"campus/bldg_{building}/floor_{floor:02d}/room_{room}/telemetry", json_payload)
-                    print("published")
                 else:
                     print("data is invalid")
+                    
                 last_published = time.ticks_ms()
-                print("temp: ", temp)
-                print("humidity", humidity)
-                print("motion", motion)
+                
         except OSError as e:
             print("network error or failed to read data from sensors")
         time.sleep(0.1)
